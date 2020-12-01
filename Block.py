23ec752072814f56c8f3f7ce9e6c0e508938c153
Thanh Dat <thanhdat02032000@gmail.com>
2020-12-01 11:44:05 +0700

    first commit

---
+++ Block.py
@@ -0,0 +1,9 @@
+class Block:
+    def __init__(self, value):
+        self.value = value
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, value):
+        self.value = value
---
+++ main.py
@@ -0,0 +1,37 @@
+import Block
+
+fileObject = open("test1.txt")
+input = fileObject.readline()
+
+para =  input.split(" ")
+n = int(para[0])
+m = int(para[1])
+print(n)
+print(m)
+
+map = []
+
+for i in range(n):
+    row = []
+    for j in range(m):
+        row.append(Block.Block(-1))
+    map.append(row)
+
+# for i in range(n):
+#     for j in range(m):
+#         map[i][j]  =  Block()
+
+rowCount = 0;
+for i in range(n-1):
+    input = fileObject.readline()
+    input = input.split(" ")
+    for j in range(m-1):
+        map[i][j].setValue(input[j])
+
+for i in range(n-1):
+    for j in range (m-1):
+        print(map[i][j].getValue())
+    print("------")
+
+       
+fileObject.close()
